85710db655174f554d5136600b7bd44b8dfc3878
han <dusrb0803@naver.com>
2023-10-10 17:39:50 +0900

    10/10

---
+++ src/main/java/com/example/demo/controller/member/dto/MemberRequestDto.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller.member.dto;
+
+import com.example.demo.repository.member.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class MemberRequestDto {
+    private String email;
+    private String password;
+    private String name;
+    private String phoneNumber;
+
+
+    public Member toEntity() {
+        return Member.builder()
+                .email(email)
+                .password(password)
+                .name(name)
+                .phoneNumber(phoneNumber)
+                .build();
+    }
+}
---
+++ src/main/java/com/example/demo/service/memo/MemoServiceImp.java
@@ -0,0 +1,38 @@
+package com.example.demo.service.memo;
+
+import com.example.demo.controller.memo.dto.MemoRequestDto;
+import com.example.demo.repository.memo.MemoRepository;
+import com.example.demo.repository.memo.entity.Memo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MemoServiceImp implements MemoService{
+    private final MemoRepository memoRepository;
+
+    @Override
+    public List<Memo> memberMemo(MemoRequestDto params) {
+        Memo memberInfo = params.toEntity();
+        return memoRepository.findByMemberId(memberInfo.getMemberId());
+    }
+
+    @Override
+    public String memoInsert(MemoRequestDto params) {
+        try{
+            memoRepository.save(params.toEntity());
+            return "success";
+        }catch(Exception e){
+            return "error";
+        }
+
+    }
+
+
+
+
+
+
+}
